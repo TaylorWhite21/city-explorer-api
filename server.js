b0eafcd672a89df38a9ae689e812e59639f21c0d
TaylorWhite21 <taylorwhite2190@gmail.com>
2021-08-05 19:23:22 -0400

    completed lab

--- server.js
+++ server.js
@@ -6,11 +6,30 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const axios = require('axios');
+const { request, response } = require('express');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT;
 
+class Forecast {
+  constructor(date, desc) {
+    this.date = date;
+    this.desc = desc;
+  }
+}
+
+class Movies {
+  constructor(title, overview, average_votes, total_votes, image_url, popularity, released_on){
+  this.title = title;
+  this.overview = overview;
+  this.average_votes = average_votes;
+  this.total_votes = total_votes;
+  this.image_url = image_url;
+  this.popularity = popularity;
+  this.released_on = released_on;
+  }
+}
 
 // app.get() specifies the route that the server should listen for
 // query parameters are used to send extra information to the backend
@@ -18,33 +37,43 @@ app.get('/weather', async (request, response) => {
   let lat = parseInt(request.query.lat);
   let lon = parseInt(request.query.lat);
   let searchQuery = request.query.city;
-  let weatherResults = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`)
- 
+  let weatherResults = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`)
   let weatherArray = [];
 
   if (weatherResults) {
       weatherResults.data.data.map(forecast => 
-      {weatherArray.push(new Weather(forecast))
+      {weatherArray.push(new Forecast(`Low of ${forecast.low_temp}, a high of ${forecast.max_temp} with ${forecast.weather.description}. date: ${forecast.valid_date}`))
     })
-    console.log(weatherArray)
     response.send(weatherArray)
   } else {
     response.status(404).send('Could not find the requested city. Please try again.');
   }
 });
 
-app.get('/*', (request, response) => {
-  response.status(500).send('Path does not exist');
+app.get('/movies', async (request, response) => {
+  let searchQuery = request.query.city;
+  let movieResults = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`)
+  if (movieResults) {
+    let mappedResults = movieResults.data.results.map(movies =>{
+      return new Movies(
+        movies.title,
+        movies.overview,
+        movies.average_votes,
+        movies.vote_count,
+        movies.poster_path,
+        movies.popularity,
+        movies.release_date,
+        );
+    })
+  response.send(mappedResults)
+} else {
+  response.status(500).send('Could not find the requested city. Please try again.');
+}
 });
 
-class Weather {
-  constructor(forecast){
-    this.low = forecast.low_temp
-    this.high = forecast.max_temp
-    this.desc = forecast.weather.description;
-    this.date = forecast.valid_date;
-  };
-}
+app.get('/*', (request, response) => {
+  response.status(404).send('Path does not exist');
+});
 
 
 
